79c790317152096d70e09b3972d7a5a9063e5f91
q945q945 <71687772+q945q945@users.noreply.github.com>
2021-01-05 02:09:25 +0800

    0105_3D_NEW

---
+++ 3D_End_107051196/Assets/Scripts/npc.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class npc : MonoBehaviour
+{
+    [Header("npc 資料")]
+    public npcData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+
+
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "robot")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+       
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "robot")
+        {
+            playerInArea = false;
+        }
+
+
+    }
+    private void Dialog()
+    {
+        for (int i = 0; i < data.dialogA.Length; i++)
+        {
+            print(data.dialogA[i]);
+
+        }
+        
+    }
+
+
+}
