ae185f39ff6771068161a14c5b2a6faaa1879d01
q945q945 <71687772+q945q945@users.noreply.github.com>
2021-01-05 15:49:40 +0800

    class_0105_3d

---
+++ 3D_End_107051196/Assets/Scripts/enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+
+    private Transform player;
+    private NavMeshAgent nav;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+        player = GameObject.Find("robot").transform;
+
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+    private void Track()
+    {
+        nav.SetDestination(player.position);
+    }
+}
--- 3D_End_107051196/Assets/Scripts/npc.cs
+++ 3D_End_107051196/Assets/Scripts/npc.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections; 
+
 
 public class npc : MonoBehaviour
 {
@@ -9,16 +11,43 @@ public class npc : MonoBehaviour
     public GameObject dialog;
     [Header("對話內容")]
     public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.1f;
 
 
 
     public bool playerInArea;
+
+    public enum npcState
+    {
+        FirstDialog, Missioning, Finish
+    }
+
+    public npcState state = npcState.FirstDialog;
+
+    //private void Start()
+    //{
+    //    StartCoroutine(Test());
+    //}
+
+    //private IEnumerator  Test()
+    //{
+    //    print("嗨~");
+    //    yield return new WaitForSeconds(1.5f);
+    //    print("嗨，我是一點五秒後");
+    //    yield return new WaitForSeconds(2);
+    //    print("嗨，又過去兩秒了");
+
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "robot")
         {
             playerInArea = true;
-            Dialog();
+            StartCoroutine(Dialog());
         }
        
 
@@ -28,15 +57,52 @@ public class npc : MonoBehaviour
         if (other.name == "robot")
         {
             playerInArea = false;
+            StopDialog();
+        }                  
+    }
+
+    /// <summary>
+    /// 停止對話
+    /// </summary>
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    /// <summary>
+    /// 開始對話
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Dialog()
+    {
+        dialog.SetActive(true);
+        
+        textContent.text = "";
+
+        textName.text = name;
+
+        string dialogString = data.dialogB;
+
+        switch (state)
+        {
+            case npcState.FirstDialog:
+                dialogString = data.dialogA;
+                break;
+            case npcState.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case npcState.Finish:
+                dialogString = data.dialogC;
+                break;
+            
         }
 
-
-    }
-    private void Dialog()
-    {
-        for (int i = 0; i < data.dialogA.Length; i++)
+        for (int i = 0; i < dialogString.Length; i++)
         {
-            print(data.dialogA[i]);
+            //print(data.dialogA[i]);
+            textContent.text += dialogString[i] + "";
+            yield return new WaitForSeconds(interval);
 
         }
         
