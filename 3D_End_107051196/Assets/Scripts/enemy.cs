71a3cb74788b6d2df8341f2b92e5b5af3bc38aef
q945q945 <71687772+q945q945@users.noreply.github.com>
2021-01-05 16:58:00 +0800

    0105_lastone

--- 3D_End_107051196/Assets/Scripts/enemy.cs
+++ 3D_End_107051196/Assets/Scripts/enemy.cs
@@ -7,13 +7,22 @@ public class enemy : MonoBehaviour
     public float speed = 3;
     [Header("停止距離"), Range(0, 50)]
     public float stopDistance = 2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float cd = 2f;
+
 
     private Transform player;
     private NavMeshAgent nav;
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
 
     private void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
 
         player = GameObject.Find("robot").transform;
 
@@ -25,9 +34,41 @@ public class enemy : MonoBehaviour
     private void Update()
     {
         Track();
+        Attack();
     }
+   /// <summary>
+   /// 攻擊
+   /// </summary>
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            timer += Time.deltaTime;
+
+            Vector3 pos = player.position;
+            pos.y = transform.position.y;
+
+            transform.LookAt(pos);
+
+            if (timer >= cd)
+            {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+            }
+
+          
+        }
+    }
+    
+    
+    
+    /// <summary>
+    /// 追蹤
+    /// </summary>
     private void Track()
     {
         nav.SetDestination(player.position);
+
+        ani.SetBool("跑步開關:" ,nav.remainingDistance > stopDistance);
     }
 }
